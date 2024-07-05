ea6d0f41596b5f5d368dfcd2908f2073afed1617
ashw2002 <aawestrick@gmail.com>
2024-07-05 17:32:19 -0400

    Created Basic Map and Crow Behavior

---
+++ BuildOneProj/Resources/Scripts/CrowBehavior.gd
@@ -0,0 +1,25 @@
+extends RigidBody2D
+var crowLoc
+@export var speed = 1
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print_debug("Start")
+	crowLoc = get_node("CrowPath/CrowLoc")
+	OnCrowSpawn()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	crowLoc.progress += speed
+	position = crowLoc.position
+
+func OnCrowSpawn():
+	crowLoc.progress_ratio = 0
+	position = crowLoc.position
+
+func _on_area_2d_area_entered(area):
+	if area.name == "EndZone":
+		print_debug('ThisBetterWork')
+		queue_free()
+
---
+++ BuildOneProj/Resources/Scripts/MapController.gd
@@ -0,0 +1,15 @@
+extends Node
+@export var CrowRef: PackedScene
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	SpawnCrow(CrowRef)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func SpawnCrow(crowType):
+	var mob = crowType.instantiate()
+	add_child(mob)
